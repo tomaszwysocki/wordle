db500f0747accdb6235326ed96205f6ec9bdba1b
Tomasz Wysocki <0xdaydream@proton.me>
2024-01-27 12:04:11 +0100

    feat: add functionality to save previous guesses

--- app/page.tsx
+++ app/page.tsx
@@ -3,50 +3,62 @@
 import { useCallback, useEffect, useState } from 'react'
 
 const Home = () => {
-    const [state, setState] = useState<string[]>([])
+    const [wordle, setWordle] = useState<string[]>([])
+    const [words, setWords] = useState<string[]>([])
+    let currentRow = words.length
 
     const handleKeystroke = useCallback(
         (e: KeyboardEvent) => {
             const char = e.key.toUpperCase()
 
-            console.log(`Key pressed: ${e.key}`)
+            // console.log(`Key pressed: ${e.key}`)
 
             if (e.key === 'Backspace') {
-                setState(prevState => prevState.slice(0, -1))
+                setWordle(prevWordle => prevWordle.slice(0, -1))
                 return
             }
 
+            if (wordle.length === 5 && e.key === 'Enter') {
+                setWords(prevWords => [...prevWords, wordle.join('')])
+                setWordle([])
+            }
+
             // return if key is not a letter or there are already 5 letters
-            if (char.length > 1 || state.length === 5) {
+            if (char.length > 1 || wordle.length === 5) {
                 return
             }
 
             if (char.toLowerCase() !== char.toUpperCase()) {
-                setState(prevState => [...prevState, char])
+                setWordle(prevWordle => [...prevWordle, char])
             }
         },
-        [state]
+        [wordle]
     )
 
     useEffect(() => {
         document.addEventListener('keydown', handleKeystroke)
+        console.log('mount')
 
         return () => {
             document.removeEventListener('keydown', handleKeystroke)
+            console.log('unmount')
         }
     }, [handleKeystroke])
 
     return (
         <main className='flex min-h-screen flex-col items-center p-24'>
-            <h1 className='text-7xl mb-16'>Wordle {state}</h1>
+            <h1 className='text-7xl mb-16'>Wordle {wordle}</h1>
             <div className='game flex flex-col gap-2'>
                 {Array.from({ length: 6 }, (_, i) => (
                     <div key={i} className='flex gap-2'>
                         {Array.from({ length: 5 }, (_, j) => (
                             <div
                                 key={j}
-                                className='size-20 bg-zinc-700 text-4xl text-center leading-[5rem]'
-                            ></div>
+                                className='size-20 bg-zinc-700 text-5xl text-center leading-[5rem]'
+                            >
+                                {words?.[i]?.[j]}
+                                {i === currentRow ? wordle[j] : ''}
+                            </div>
                         ))}
                     </div>
                 ))}
